3ec86d0fd063db2de2323f42e4eccace24a7402d
BurdenBear <541795600@qq.com>
2018-03-26 10:44:55 +0800

    fix jaqs_fxdayu.data.__init__.py

--- jaqs_fxdayu/data/__init__.py
+++ jaqs_fxdayu/data/__init__.py
@@ -1,5 +1,14 @@
-from .dataview import DataView
+# encoding: utf-8
+
+"""
+Modules relevant to data.
+
+"""
+
+from .dataapi import DataApi
+from .dataservice import RemoteDataService, DataService
+from .dataview import DataView, EventDataView
 from .py_expression_eval import Parser
 
 # we do not expose align and basic
-__all__ = ['DataView', "Parser"]
+__all__ = ['DataApi', 'DataService', 'RemoteDataService', 'DataView', 'Parser', 'EventDataView']
